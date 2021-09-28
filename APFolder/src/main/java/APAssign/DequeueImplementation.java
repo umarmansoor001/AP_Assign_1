66e4578c72e10cc8538e0d462ade005239e65fca
umarmansoor001 <90560001+umarmansoor001@users.noreply.github.com>
2021-09-28 22:24:24 +0500

    Update DequeueImplementation.java

--- APFolder/src/main/java/APAssign/DequeueImplementation.java
+++ APFolder/src/main/java/APAssign/DequeueImplementation.java
@@ -1,5 +1,22 @@
 package APAssign;
 
-public class DequeueImplementation {
+//Program to execute Dequeue.
+public class DequeueImplementation{
+	public static void main(String [] args){
+		Dequeue queue = new Dequeue(5);
+		queue.addRight(10);
+		queue.display();
 
+		queue.addLeft(20);
+		queue.display();
+
+		queue.addRight(12);
+		queue.display();
+
+		queue.addLeft(18);
+		queue.display();
+	
+		queue.addRight(16);
+		queue.display();
+	}
 }
